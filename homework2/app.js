27da07ccce2046d55f6a092679cad99bc2adfb56
Alexander Zhuravlov <alexander.zhuravlov@dev-pro.net>
2017-01-22 18:08:53 +0200

    Added homework3 folder

--- homework2/app.js
+++ homework2/app.js
@@ -18,21 +18,72 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on http://localhost:' + app.get('port') );
 });
 
-/* Default Routes */
+/* Default Home Route */
 app.get('/', function (req, res) {
     res.send('<html><body><h1>My web app http API! Version ' + apiVersion + '</h1></body></html>');
 });
 
-app.post('/post', function (req, res) {
-    res.send('<html><body><h1>Post body' + req + '</h1></body></html>');
+/* POSTS Routes */
+app.route('/api/:apiVersion/posts/')
+    .get(function (req, res) {
+        res.send(req.params)
+    })
+    .post(function (req, res) {
+        res.send(req.params)
+    });
+app.route('/api/:apiVersion/posts/:postId/')
+    .get(function (req, res) {
+        res.send(req.params)
+    })
+    .post(function (req, res) {
+        res.send(req.params)
+    })
+    .delete(function (req, res) {
+        res.send(req.params)
+    })
+    .put(function (req, res) {
+        res.send(req.params)
+    });
+
+/* USERS Routes */
+app.route('/api/:apiVersion/users/')
+    .get(function (req, res) {
+        res.send(req.params)
+    })
+    .post(function (req, res) {
+        res.send(req.params)
+    });
+app.route('/api/:apiVersion/users/:postId/')
+    .get(function (req, res) {
+        res.send(req.params)
+    })
+    .post(function (req, res) {
+        res.send(req.params)
+    })
+    .delete(function (req, res) {
+        res.send(req.params)
+    })
+    .put(function (req, res) {
+        res.send(req.params)
+    });
+
+
+/* Default Not Found Route */
+app.all('/api/*', function (req, res) {
+    res.sendStatus(404);
 });
 
+
+
+
+
 /* API GET Method */
-app.get('/api/:apiVersion/*', function (req, res) {
+/*
+app.get('/api/:apiVersion/!*', function (req, res) {
     res.type('json');
     helpers.getData(req.path).then(data => {
         res.json(data);
     }).catch(error =>{
         res.json(error);
     });
-});
+});*/
--- homework2/constants.js
+++ homework2/constants.js
@@ -1,10 +1,3 @@
-const postsModel = require('./posts');
-const usersModel = require('./users');
-
 module.exports = {
-    APP_PORT: 3333,
-    AVAILABLE_PARAMS:{
-        posts: postsModel,
-        users: usersModel
-    }
+    APP_PORT: 3333
 };
